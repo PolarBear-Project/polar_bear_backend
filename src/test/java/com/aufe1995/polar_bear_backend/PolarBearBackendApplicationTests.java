081eb163387a15e258fa4ee55b168eb47bfdaf7e
gold <aufe1995@gmail.com>
2024-04-19 21:47:46 +0900

    init commit

---
+++ src/test/java/com/aufe1995/polar_bear_backend/PolarBearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aufe1995.polar_bear_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarBearBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
